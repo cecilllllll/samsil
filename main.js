cfac830021447ff63467482442cafde51aed5626
cecilllllll <94329566+cecilllllll@users.noreply.github.com>
2021-11-16 08:57:34 +0900

    .d

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   $('#fullpage').fullpage({
     sectionSelector: '.section',
     slideSelector: '.horizontal-scrolling',
+    // slideSelector: '.section',
     navigation: true,
     slidesNavigation: true,
     css3: true,
